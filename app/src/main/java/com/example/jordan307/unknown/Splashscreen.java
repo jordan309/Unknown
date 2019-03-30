440023c6310fbe7befaae538fd5ac5893d659924
Jordan <jtwood@deakin.edu.au>
2019-03-30 20:43:25 +1100

    Splash Screen activity page complete with animation and text of the app's name.

---
+++ app/src/main/java/com/example/jordan307/unknown/Splashscreen.java
@@ -0,0 +1,40 @@
+package com.example.jordan307.unknown;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class Splashscreen extends AppCompatActivity {
+
+    private ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        iv = (ImageView) findViewById(R.id.iv);
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.splash_transition);
+        iv.startAnimation(myanim);
+
+        final Intent i = new Intent(this,MainActivity.class);
+
+        Thread timer = new Thread(){
+            public void run () {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+
+        };
+            timer.start();
+    }
+}
